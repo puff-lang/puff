757c6f323841b01ea56d75b099ef5eaf519f8b75
Debjit Biswas <debjit@fastmail.fm>
2016-04-25 14:49:25 +0530

    Add REPL support

--- src/main/main.go
+++ src/main/main.go
@@ -3,15 +3,36 @@ package main
 import (
 	"fmt"
     "os"
+    "io"
 	"io/ioutil"
+    "bufio"
     "core"
     "compile"
+    "strings"
 )
 
-func main() {
-    action := os.Args[1]
-    fileName := os.Args[2]
+func replLoop() {
+    reader := bufio.NewReader(os.Stdin)
+    // H := jit.addModule(module)
+    var globals string
+    for {
+        fmt.Print("puff> ")
+        text, err := reader.ReadString('\n')
 
+        if err == io.EOF {
+            break
+        }
+
+        if strings.HasPrefix(text, "fn ") {
+            globals = globals + "\n" + text
+        } else {
+            program := compile.Translate(globals + "\n" + "fn main() => " + text)
+            core.ShowStates(core.Compile(program))
+        }
+    }
+}
+
+func compileProgramFromFile(fileName string) core.GmState {
     b, err := ioutil.ReadFile(fileName)
 
     fmt.Println(string(b))
@@ -35,17 +56,26 @@ func main() {
     }
 
     if mainFound == false {
-        fmt.Println("No main function found in program. Aborting.")
-        return
+        panic("No main function found in program. Aborting.")
     }
 
-    contentProgram := core.Compile(program)
+    return core.Compile(program)
+}
+
+func main() {
+    action := os.Args[1]
 
     switch action {
     case "run":
-        core.ShowStates(contentProgram)
+        fileName := os.Args[2]
+        core.ShowStates(compileProgramFromFile(fileName))
+    case "repl":
+        fmt.Println("Starting puff REPL")
+        replLoop()
+        fmt.Println("\nGoodbye! Thanks for using puff.")
     default:
-        compile.SaveLLVMIR(compile.GenLLVMIR(contentProgram))
+        fileName := os.Args[2]
+        compile.SaveLLVMIR(compile.GenLLVMIR(compileProgramFromFile(fileName)))
     }
 }
 
